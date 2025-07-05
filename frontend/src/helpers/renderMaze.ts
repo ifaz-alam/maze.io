4923d2d3873476117b0d24790a461d861770e56f
Ifaz Alam <ifazalam33@gmail.com>
2025-07-05 14:25:32 -0400

    Implement structure for Randomized Depth First Search using iteration and interfaces for stack items.

--- frontend/src/helpers/renderMaze.ts
+++ frontend/src/helpers/renderMaze.ts
@@ -1,7 +1,16 @@
 import { Application, Container, ContainerChild, Graphics, removeStructAndGroupDuplicates, Renderer } from "pixi.js";
+import { shuffleArray } from "./arrayUtils";
 
 interface MazeCell {
     neighbours: number[][];
+}
+
+interface dfsStackItem {
+    row: number;
+    col: number;
+    isRoot: boolean;
+    rowQueuedFrom?: number;
+    colQueuedFrom?: number;
 }
 
 const TILE_SIZE: number = 20;
@@ -26,12 +35,12 @@ export const renderMaze = (app: Application<Renderer>) => {
 
     const mazeContainer: Container<ContainerChild> = new Container();
     app.stage.addChild(mazeContainer);
-    getMaze(mazeHeight, mazeWidth);
+    const maze: MazeCell[][] = getMaze(mazeHeight, mazeWidth);
 
     // Let us process the maze container with the Graphics API
     // Without loss of generality, maze tiles will be indexed from 0 to mazeWidth - 1, since we are drawing from 0-indexed positions
-    // const block: Graphics = new Graphics().rect((mazeWidth - 1) * TILE_SIZE, 0, TILE_SIZE, TILE_SIZE).fill(0xffffff);
-    // app.stage.addChild(block);
+    const block: Graphics = new Graphics().rect((mazeWidth - 1) * TILE_SIZE, 0, TILE_SIZE, TILE_SIZE).fill(0xffffff);
+    app.stage.addChild(block);
     console.log(app.renderer.height);
 };
 
@@ -41,22 +50,18 @@ export const renderMaze = (app: Application<Renderer>) => {
  * @param num_cols - The number of columns the maze should have
  * @returns A matrix where each cell maintains its own list of valid neighbours
  */
-const getMaze = (num_rows: number, num_cols: number) => {
-    const visited: Boolean[][] = Array(num_rows)
-        .fill(0)
-        .map(() => Array(num_cols).fill(false));
+const getMaze = (num_rows: number, num_cols: number): MazeCell[][] => {
+    const generateMazeWithDepthFirstSearch = (rootRow: number, rootCol: number) => {
+        const visited: Boolean[][] = Array(num_rows)
+            .fill(0)
+            .map(() => Array(num_cols).fill(false));
 
-    const maze: MazeCell[][] = Array(num_rows)
-        .fill(0)
-        .map(() => Array(num_cols).fill({ neighbours: [] }));
+        // Randomized DFS will fill the metadata in this variable
+        const maze: MazeCell[][] = Array(num_rows)
+            .fill(0)
+            .map(() => Array(num_cols).fill({ neighbours: [] }));
 
-    const dfs = (row: number, col: number, visited: Boolean[][]) => {
-        const performBacktrack: Boolean = 0 <= row && row < num_rows && 0 <= num_cols && num_cols < num_rows;
-        if (performBacktrack) {
-            return;
-        }
-
-        visited[row][col] = true;
+        const stack: dfsStackItem[] = [{ row: rootRow, col: rootCol, isRoot: true }];
 
         const deltas: [number, number][] = [
             [0, -1],
@@ -65,16 +70,55 @@ const getMaze = (num_rows: number, num_cols: number) => {
             [1, 0],
         ];
 
-        // Determine where we can go
-        const validNeighbours: [number, number][] = deltas
-            .map(([dx, dy, dir]) => [row + dx, col + dy] as [number, number])
-            .filter(([new_row, new_col]) => {
-                const isWithinBounds: Boolean = 0 <= new_row && new_row < num_rows && 0 <= new_col && new_col < num_cols;
-                return isWithinBounds && visited[new_row][new_col] == false;
-            });
+        while (stack.length > 0) {
+            const currCell: dfsStackItem = stack.pop() as dfsStackItem;
+            const currRow: number = currCell.row;
+            const currCol: number = currCell.row;
+            const isRoot: boolean = currCell.isRoot;
 
-        // Randomize order of neighbours
+            // The root will need to get its metadata updated eventually.
+            // To avoid losing information when simulating the call stack, we make each neighbour that gets pushed in the stack know who enqueued it (Check randomized neighbour process logic below).
+            // That way, the neighbour list of the respective nodes are maintained and updated when necessary.
+            if (!isRoot) {
+                const rowQueuedFrom: number = currCell.rowQueuedFrom as number;
+                const colQueuedFrom: number = currCell.colQueuedFrom as number;
+
+                // Update data bidirectionally
+                maze[rowQueuedFrom][colQueuedFrom].neighbours.push([currRow, currCol]);
+                maze[currRow][currCol].neighbours.push([rowQueuedFrom, colQueuedFrom]);
+            }
+
+            // We only push in neighbours within bounds so really we just need to check visited only.
+            const performBacktrack: Boolean = visited[currRow][currCol] == true;
+            if (performBacktrack) {
+                continue;
+            }
+
+            // Determine where we can go
+            const validNeighbours: [number, number][] = deltas
+                .map(([dx, dy]) => [currRow + dx, currCol + dy] as [number, number])
+                .filter(([new_row, new_col]) => {
+                    const isWithinBounds: Boolean = 0 <= new_row && new_row < num_rows && 0 <= new_col && new_col < num_cols;
+                    return isWithinBounds && visited[new_row][new_col] == false;
+                });
+
+            // Randomize order of neighbours
+            const randomizedNeighbours = shuffleArray(validNeighbours);
+
+            for (const neighbour of randomizedNeighbours) {
+                const neighbourRow: number = neighbour[0];
+                const neighbourCol: number = neighbour[1];
+
+                if (!visited[neighbourRow][neighbourCol]) {
+                    stack.push({ row: neighbourRow, col: neighbourCol, isRoot: false, rowQueuedFrom: currRow, colQueuedFrom: currCol });
+                }
+            }
+        }
+
+        return maze;
     };
 
-    dfs(0, 0, visited);
+    // TODO: Start generation process from a random cell within bounds to possibly introduce more variation.
+    const maze: MazeCell[][] = generateMazeWithDepthFirstSearch(0, 0);
+    return maze;
 };
